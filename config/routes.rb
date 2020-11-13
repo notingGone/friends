1bdeb91e410fc7a62b0d9e99514f2e2c648538ec
notingGone <elliottcwoodward@gmail.com>
2020-11-13 16:02:53 -0500

    Add bootstrap test page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'home/index'
   root 'home#index'
+  get 'bootstrap', to: 'home#bootstrap'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
